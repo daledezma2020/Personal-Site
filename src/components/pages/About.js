cf37b37372392a703f29e3ef10595935c49b7f41
daledezma2020 <daledezma2020@gmail.com>
2023-01-08 19:13:48 -0500

    Adjusted a few proportions

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,7 +16,8 @@ function About() {
        in high school it would take a lot of hard work to get to where I wanted to be. In college, though still fun, programming has become hard work and as a result I've picked 
        up a hobby in music to supplement this. Now, having been playing guitar, learning music theory and studying computer science over the past 3 years, I am certain 
        I want to incorporate my creative thinking from music into my software development work one way or another.
-      
+      </p>
+      <p>
       Currently I am seeking employment in either research or internships involving software development. I am really interested in front end web development, however after 
       having taken a wide variety of classes at the University of Rochester in various languages of low and high level, I know that I can get important skills from almost any 
       sort of coding opportunity.
