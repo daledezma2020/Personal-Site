8665ab19deb82e8436966d450206962639197833
Daniel Ledezma <dledezma@u.rochester.edu>
2023-01-30 18:33:21 -0500

    Fixed Warnings with resume.pdf, removed pagenumber counter.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,23 +3,14 @@ import '../../App.css';
 import './Resume.css';
 import { useState } from 'react';
 import { Document, Page} from 'react-pdf'
-// import { Helmet } from "react-helmet";
-
-
-
 
 function Resume() {
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  const [pageNumber] = useState(1);
 
   return (
       <div className = 'container'>
-      <Document file="./2023Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+
+      <Document file="./2023Resume.pdf">
         <Page pageNumber={pageNumber} />
       </Document>
     </div>
