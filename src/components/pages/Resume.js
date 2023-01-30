099fb30741ff395a15715b0ecd41cf3e3e16aa2c
Daniel Ledezma <dledezma@u.rochester.edu>
2023-01-30 18:17:14 -0500

    uninstalled yarn, fixed pdf functionality

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../../App.css';
 import './Resume.css';
-// import { useState } from 'react';
-// import { Document, Page} from 'react-pdf'
+import { useState } from 'react';
+import { Document, Page} from 'react-pdf'
 // import { Helmet } from "react-helmet";
 
 
@@ -10,24 +10,18 @@ import './Resume.css';
 
 function Resume() {
 
-  // const [numPages, setNumPages] = useState(null);
-  // const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  // function onDocumentLoadSuccess({ numPages }) {
-  //   setNumPages(numPages);
-  // }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
       <div className = 'container'>
-      <img src = '/images/2023Resume-1.png' alt = "My resume" className = 'resumePic'/>
-
-      {/* <Document file="./2023Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      <Document file="./2023Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p> */}
-      
     </div>
   );
 }
